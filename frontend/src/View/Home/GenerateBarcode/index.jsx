a882d95a80dbe7296a6c019de4e1dc7901a2d153
pembe <pembe@ADMIN8PEMBELIAN>
2025-07-18 15:57:44 +0700

    Design Generate Done Handling Update Delete InProgress

--- frontend/src/View/Home/GenerateBarcode/index.jsx
+++ frontend/src/View/Home/GenerateBarcode/index.jsx
@@ -11,16 +11,63 @@ const GenerateBarcode = () => {
       send_date: "11/07/2025",
       DNID: ["2025/07/0255", "2025/07/0253", "2025/07/0256", "2025/07/0257"],
     },
-  ]);
+   ]);
+
+  const [delivr, setDelivr] = useState({ send_date: "", DNID: "" });
+
+  const handleChangeField = (e) => {
+    const { name, value } = e.target;
+    if (name == "send_date") {
+      const send_date = value;
+      setDelivr({ ...delivr, send_date });
+    } else if (name == "DNID") {
+      const DNID = value;
+      setDelivr({ ...delivr, DNID });
+    }
+  };
+
+  const handleSubmit = () => {
+    let { send_date, DNID } = delivr;
+    if (send_date && DNID) {
+      const found_deliv = list.findIndex((v) => v.send_date == send_date);
+      //if the delivery found on list
+      if (found_deliv > -1) {
+        const cDNID = list[found_deliv].DNID.find((v) => v == DNID);
+        if (!cDNID) {
+          const nDNID = [...list[found_deliv].DNID, DNID];
+          list[found_deliv] = { send_date, DNID: nDNID };
+          setList([...list]);
+        }
+        //init if not found
+      } else {
+        setList([...list, { send_date, DNID: [DNID] }]);
+      }
+      console.log(list);
+    } else {
+      console.log("send date or dnid should be filled");
+    }
+  };
+
+  const handleUpdate = (id) => {
+    console.log(id);
+  };
+
+  const handleDelete = (id) => {
+    console.log(id);
+  };
 
   const listContentComp = list.map((v) => {
     const { send_date, DNID } = v;
-    const dnComp = DNID.map((v) => {
+    const dnComp = DNID.map((v,i) => {
       return (
         <div className="flex flex-row justify-between">
-          <div className="bg-yellow-500 w-9/12 flex justify-center content-center">{v}</div>
+          <div className="w-9/12 flex justify-center pt-2">{v}</div>
           <div className="flex flex-row">
-            <button className="btn btn-square btn-ghost">
+            <button
+              className="btn btn-square btn-ghost"
+              name="updateDN"
+              onClick={()=>handleUpdate(i)}
+            >
               <svg
                 className="size-[1.2em]"
                 xmlns="http://www.w3.org/2000/svg"
@@ -33,11 +80,16 @@ const GenerateBarcode = () => {
                   fill="none"
                   stroke="currentColor"
                 >
-                  <path d="M6 3L20 12 6 21 6 3z"></path>
+                  <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                  <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1l1-4l9.5-9.5z"></path>
                 </g>
               </svg>
             </button>
-            <button className="btn btn-square btn-ghost">
+            <button
+              className="btn btn-square btn-ghost"
+              name="deleteDN"
+              onClick={()=>handleDelete(i)}
+            >
               <svg
                 className="size-[1.2em]"
                 xmlns="http://www.w3.org/2000/svg"
@@ -50,7 +102,10 @@ const GenerateBarcode = () => {
                   fill="none"
                   stroke="currentColor"
                 >
-                  <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
+                  <path d="M3 6h18"></path>
+                  <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                  <path d="M10 11v6"></path>
+                  <path d="M14 11v6"></path>
                 </g>
               </svg>
             </button>
@@ -72,19 +127,37 @@ const GenerateBarcode = () => {
 
   return (
     <>
-      <label className="input">
+      <label className="input w-full">
         <span className="label">Delivery Date</span>
-        <input type="date" />
-      </label>
-
-      <label className="floating-label">
-        <span>SJ ID</span>
         <input
-          type="text"
-          placeholder="YYYY/MM/NNNN"
-          className="input input-md"
+          type="date"
+          value={delivr.send_date}
+          name="send_date"
+          onChange={handleChangeField}
         />
       </label>
+
+      <div className="flex flex-row justify-between">
+        <label className="floating-label w-full pr-4">
+          <span>SJ ID</span>
+          <input
+            type="text"
+            placeholder="YYYY/MM/NNNN"
+            className="input input-md w-full"
+            name="DNID"
+            value={delivr.DNID}
+            onChange={handleChangeField}
+          />
+        </label>
+
+        <button
+          className="btn btn-primary"
+          name="submit_delivr"
+          onClick={()=>handleSubmit()}
+        >
+          Submit
+        </button>
+      </div>
 
       <ul className="list bg-base-100 rounded-box shadow-md overflow-auto max-h-9/12 min-h-9/12">
         {listContentComp}
--- frontend/src/View/Home/index.jsx
+++ frontend/src/View/Home/index.jsx
@@ -5,33 +5,34 @@ import ShowBarcode from "./ShowBarcode";
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center h-screen w-screen">
-      <div className="flex flex-col h-7/12 min-w-80 w-7/12 card shadow-sm p-4">
-        <div className="flex flex-row text-center font-semibold shadow-sm h-1/12">
+    <div className="flex justify-center items-center h-screen w-screen bg-base-300">
+      <div className="flex flex-col h-7/12 min-w-80 w-7/12 card shadow-sm p-4 bg-base-100">
+        <div className="flex flex-row text-center font-semibold h-1/12">
           <Link
             to={""}
-            className="flex-1/2 pt-3 pb-3 hover:animate-pulse hover:shadow-sm"
+            className="flex-1/2 pt-2.5 pb-3 hover:animate-pulse shadow-md card bg-primary-200 text-base-content"
           >
             Generate
           </Link>
+          <div className="divider-horizontal divider"></div>
           <Link
             to={"show"}
-            className="flex-1/2 pt-3 pb-3 hover:animate-pulse hover:shadow-sm"
+            className="flex-1/2 pt-2.5 pb-3 hover:animate-pulse shadow-accent-content card bg-primary-200 text-base-content"
           >
             Show
           </Link>
         </div>
         <div className="flex flex-row h-11/12">
-          <div className="flex-1/2 flex flex-col justify-between ">
-            <div className="h-11/12 flex flex-col justify-between">
+          <div className="flex-1/2 flex flex-col justify-between p-4">
+            <div className="h-11/12 flex flex-col justify-between pb-3">
               <Routes>
                 <Route index element={<GenerateBarcode />} />
                 <Route path="show" element={<ShowBarcode />} />
               </Routes>
             </div>
             <div className="flex flex-row justify-end h-1/12">
-              <button className="btn btn-primary mr-4">Submit</button>
-              <button className="btn btn-secondary">Clear</button>
+              <button className="btn btn-primary mr-4 h-full">Submit</button>
+              <button className="btn btn-secondary h-full">Clear</button>
             </div>
           </div>
           <div className="flex-1/2">test</div>
